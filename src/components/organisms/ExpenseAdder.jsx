d794f9a8b5da9c233dfc0bf9ee8bbb999b510e5e
clickerr_ <bartekswiech1103@gmail.com>
2024-07-14 19:00:31 -0700

    Implement Delete

--- src/components/organisms/ExpenseAdder.jsx
+++ src/components/organisms/ExpenseAdder.jsx
@@ -4,16 +4,20 @@ import "../../styles/expenseAdder.css";
 const ExpenseAdder = ({onClose, addNewExpense}) => {
 
     const [name, setName] = useState("");
-
     const [amount, setAmount] = useState(0);
-
     const [category, setCategory] = useState("");
-
     const [date, setDate] = useState(new Date());
     const [dateDisplay, setDateDisplay] = useState("");
+    const [errorText, setErrorText] = useState("");
+    
 
     const addExpense = () => {
-        
+        if((amount === null || amount <= 0)) {
+            setErrorText("Please enter a valid positive number");
+            return;
+        }
+        setErrorText("");
+            
         const newExpense = {name: name, amount: amount, category: category, date: date};
         console.log(newExpense);
         addNewExpense(newExpense);
@@ -25,11 +29,19 @@ const ExpenseAdder = ({onClose, addNewExpense}) => {
                 <button className="close-button" onClick={onClose}>X</button>
             </div>
             <div className="adder-container">
+                <label htmlFor="name">Name</label>
                 <input onChange={(event) => {setName(event.target.value)}} type="text" className="form-input" name="name" value={name} placeholder="Name"></input>
+                <label htmlFor="amount">Amount<span className="red-text">*</span></label>
                 <input onChange={(event) => {setAmount(event.target.value)}} type="number" className="form-input" name="amount" value={amount} placeholder="Amount"></input>
+                <label htmlFor="category">Category</label>
                 <input onChange={(event) => {setCategory(event.target.value)}} type="text" className="form-input" name="category" value={category} placeholder="Category"></input>
-                <input onChange={(event) => {setDateDisplay(event.target.value), setDate(new Date(event.target.value))}} type="date" className="form-input" name="name" value={dateDisplay} placeholder="Name"></input>
+                <label htmlFor="date">Date</label>
+                <input onChange={(event) => {setDateDisplay(event.target.value), setDate(new Date(event.target.value))}} type="date" className="form-input" name="date" value={dateDisplay}></input>
+                <label>{errorText}</label>
                 <button onClick={() => addExpense()}>Submit</button>
+            </div>
+            <div>
+                <span>* signify required fields</span>
             </div>
         </div>
     );
--- src/components/views/Dashboard.jsx
+++ src/components/views/Dashboard.jsx
@@ -2,11 +2,12 @@ import { useEffect, useState, useContext } from 'react'
 import Navbar from "../organisms/Navbar";
 import '../../styles/master.css';
 import '../../styles/dashboard.css';
+import editLogo from "../../assets/edit-icon.svg";
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../AuthenticationContext';
 import ExpenseAdder from '../organisms/ExpenseAdder';
 import { db } from '../../firebase-config';
-import { getDocs, collection, addDoc } from 'firebase/firestore';
+import { getDocs, getDoc, collection, addDoc, deleteDoc, doc } from 'firebase/firestore';
 
 const Dashboard = () => {
   
@@ -15,56 +16,72 @@ const Dashboard = () => {
   const [expenseList, setExpenseList] = useState([]);
   const [newExpense, setNewExpense] = useState(null);
   const [yearDropdownEnabled, setYearDropdownEnabled] = useState(false);
+  const [monthDropdownEnabled, setMonthDropdownEnabled] = useState(false);
   const [selectedYear, setSelectedYear] = useState(null);
   const [showExpenseAdder, setShowExpenseAdder] = useState(false);
+  const [isDeleteMode, setDeleteMode] = useState(false);
   
   const [todos, setTodos] = useState([]);
  
+  const categories = ["Food", "Gas", "Grocery", "Personal", "Subscriptions"];
+  const years = ["2019", "2020", "2021", "2022", "2023", "2024"];
+  const months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sept","Oct","Nov","Dec"];
+  let dates = []
+
   const fetchPost = async (year) => {
     await getDocs(collection(db, "expenses", user.uid, year))
       .then((querySnapshot)=>{               
           const newData = querySnapshot.docs.map( (doc) => { return {...doc.data(), docId: doc.id} });
-          console.log(newData);
-          console.log(`FETCHPOST Current Expense List `);
-          console.log(expenseList);
           setExpenseList([...expenseList, ...newData]);
-          // setExpenseList([...expenseList, ...sampleExpenseList]);
+      }).catch( (error) => {console.log(error);})
+  }
+
+  const refreshExpenseList = async (year) => {
+    await getDocs(collection(db, "expenses", user.uid, year))
+      .then((querySnapshot)=>{               
+          const newData = querySnapshot.docs.map( (doc) => { return {...doc.data(), docId: doc.id} });
+          setExpenseList(newData);
       }).catch( (error) => {console.log(error);})
   }
     
   const addNewExpense = async (expenseItem) => {
+    expenseItem.userId = user.uid;
     
-    console.log(user.uid)
-    console.log(expenseItem);
-    
-    // Add a new document with a generated id.
     const docRef = await addDoc(collection(db, "expenses", user.uid, expenseItem.date.getFullYear().toString()), {
       name: expenseItem.name,
       amount: expenseItem.amount,
       category: expenseItem.category,
       date: expenseItem.date.toJSON(),
-      userId: user.uid,
-    }).then( () => {fetchPost(new Date().getFullYear().toString())}).catch( (error) => {console.log(error)});
+      userId: expenseItem.userId
+    }).then( (newDoc) => {
+      expenseItem.docId = newDoc.id;
+      setExpenseList([...expenseList, expenseItem])
+    }).catch( (error) => {console.log(error)});
       
   }
-   
-  useEffect(()=>{
-      
-      fetchPost(new Date().getFullYear().toString());
-  }, [])
 
+  const deleteExpense = async (expenseToDelete) => {
 
-  const categories = ["Food", "Gas", "Grocery", "Personal", "Subscriptions"];
-  const years = ["2019", "2020", "2021", "2022", "2023", "2024"];
-  let dates = []
+    const expenseYear = new Date(expenseToDelete.date).getFullYear();
+    const docRef = await deleteDoc(doc(db, "expenses", expenseToDelete.userId.toString(), expenseYear.toString(), expenseToDelete.docId.toString()))
+    .then( () => {deleteFromExpenseList(expenseToDelete)} ).catch( (error) => {console.error(error)});
+  }
+
+  const deleteFromExpenseList = (expense) => {
+    let expenseListCopy = [...expenseList];
+    let updatedExpenseList = expenseListCopy.filter( (data) => { return data.docId.toString() !== expense.docId.toString() });
+    setExpenseList(updatedExpenseList);
+  }
   
-  useEffect(()=>{
-    // console.log(user);
-    
+  useEffect(() => {
     if(user === null)
     {
       console.log("Redirecting");
       navigate("/login");
+    }
+    else {
+      console.log("Fetching");
+      fetchPost(new Date().getFullYear().toString());
     }
   },[user])
   
@@ -72,6 +89,12 @@ const Dashboard = () => {
     setSelectedYear(years[yearIndex]);
     console.log(yearIndex);
     setYearDropdownEnabled(false);
+  }
+
+  const handleSelectMonth = (monthIndex) => {
+    setSelectedYear(months[monthIndex]);
+    console.log(monthIndex);
+    setMonthDropdownEnabled(false);
   }
 
   const renderYears = () => {
@@ -84,9 +107,17 @@ const Dashboard = () => {
     })
   }
 
+  const renderMonths = () => {
+    return months.map((element, id) => {
+      return (
+        <div className='year-entry' onClick={() => handleSelectMonth(id)}>
+          <span className='year-entry-text'>{element}</span>
+        </div>
+      )
+    })
+  }
 
   const renderListItems = () => {
-    
     
     if(!selectedYear)
     {
@@ -97,35 +128,9 @@ const Dashboard = () => {
         console.log(selectedYear);
         return( 
           <div key={id} className="element">
-            <div>
-              <h3>{element.name}</h3>
-            </div>
-            <div>
-              <span>{`${elementDate.getMonth() + 1}-${elementDate.getDate()}-${elementDate.getFullYear()}`}</span>
-            </div>
-            <div>
-              <span>{`${element.category}`}</span>
-            </div>
-            <div>
-              <span>{`$${element.amount}`}</span>
-            </div>
-          </div> 
-        )
-      })
-    }
-    else {
-      return expenseList.map((element, id) => {
-        let elementDate = new Date(element.date)
-        console.log(element);
-        console.log(elementDate.getFullYear());
-        console.log(selectedYear);
-        if(elementDate.getFullYear() == selectedYear)
-        {
-          return(
-            <div key={id} className="element">
-              <div>
-                <h3>{element.name}</h3>
-              </div>
+            
+            <h3 className='expense-name'>{element.name}</h3>
+            <div className='expense-content'>
               <div>
                 <span>{`${elementDate.getMonth() + 1}-${elementDate.getDate()}-${elementDate.getFullYear()}`}</span>
               </div>
@@ -135,6 +140,55 @@ const Dashboard = () => {
               <div>
                 <span>{`$${element.amount}`}</span>
               </div>
+            </div>
+            
+            <div className='edit-delete'>
+              <div>
+                <button className="edit-logo"><img src={editLogo}/></button>
+              </div>
+              <div>
+                <button disabled={!isDeleteMode} onClick={() => deleteExpense(element)}>Delete</button>
+              </div>
+            </div>
+            
+          </div> 
+        )
+      })
+    }
+    else {
+      
+      return expenseList.map((element, id) => {
+        let elementDate = new Date(element.date)
+        console.log(element);
+        console.log(elementDate.getFullYear());
+        console.log(selectedYear);
+        if(elementDate.getFullYear() == selectedYear)
+        {
+          return( 
+            <div key={id} className="element">
+              
+              <h3 className='expense-name'>{element.name}</h3>
+              <div className='expense-content'>
+                <div>
+                  <span>{`${elementDate.getMonth() + 1}-${elementDate.getDate()}-${elementDate.getFullYear()}`}</span>
+                </div>
+                <div>
+                  <span>{`${element.category}`}</span>
+                </div>
+                <div>
+                  <span>{`$${element.amount}`}</span>
+                </div>
+              </div>
+              
+              <div className='edit-delete'>
+                <div>
+                  <button className="edit-logo"><img src={editLogo}/></button>
+                </div>
+                <div>
+                  <button disabled={!isDeleteMode} onClick={() => deleteExpense(element)}>Delete</button>
+                </div>
+              </div>
+              
             </div> 
           )
         }
@@ -166,6 +220,10 @@ const Dashboard = () => {
           <div>
             <button onClick={() => {setShowExpenseAdder(!showExpenseAdder)}}>+</button>
           </div>
+          <div>
+            <label htmlFor="Delete Mode">Delete Mode</label>
+            <input id="Delete Mode" name="Delete Mode" type="checkbox" onChange={() => setDeleteMode(!isDeleteMode)} value={isDeleteMode} />
+          </div>
           <div className='header-button-row'>
             <div className='year-dropdown'>
               <button className='year-button' onClick={() => {setYearDropdownEnabled(!yearDropdownEnabled )}}>Year</button>
@@ -173,11 +231,19 @@ const Dashboard = () => {
                 {renderYears()}
               </div>) : (<></>)}
             </div>
+            <div className='year-dropdown'>
+              <button className='year-button' onClick={() => {setMonthDropdownEnabled(!monthDropdownEnabled)}}>Month</button>
+              {monthDropdownEnabled ? (<div className='year-content'>
+                {renderMonths()}
+              </div>) : (<></>)}
+            </div>
           </div>
           <div className='expense-list-header'>
             <span>Expense Name</span>
+            <span>Expense Date</span>
             <span>Expense Category</span>
             <span>Amount</span>
+            <span>Options</span>
           </div>
           <div className='list-of-expenses'>
             {renderListItems()}
