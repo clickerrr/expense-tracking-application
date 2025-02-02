ed5abd92d7feb2c475a79c638d2ce0efad571eef
Bartek Swiech <bartekswiech1103@gmail.com>
2025-02-02 11:08:14 -0700

    Create budget screen and have it display after new budget 'created'. Mock data.

---
+++ src/components/organisms/budgeting/BudgetCategoryTable.tsx
@@ -0,0 +1,121 @@
+import {useEffect, useState} from 'react';
+import '@/styles/budgettable.css';
+import editLogo from '@/assets/edit-icon.svg';
+import BudgetCategoryItem from '@/types/BudgetCategoryItem';
+
+interface BudgetTableProps {
+	title: string;
+	budgetData: BudgetCategoryItem[];
+	onEdit: () => void;
+}
+
+const BudgetCategoryTable = ({title, budgetData, onEdit}: BudgetTableProps) => {
+	const headColumns: string[] = ['Category Title', 'Planned', 'Actual', 'Difference'];
+
+	useEffect(() => {
+		sumTotal('planned', setTotalPlanned);
+		sumTotal('actual', setTotalActual);
+		sumDifference();
+	}, [title, budgetData]);
+
+	const [totalPlanned, setTotalPlanned] = useState<number>(0);
+	const [totalActual, setTotalActual] = useState<number>(0);
+	const [totalDiff, setTotalDiff] = useState<number>(0);
+
+	const sumTotal = (field: string, setter: (data: number) => void) => {
+		let totalSum = 0;
+		budgetData.forEach((element: BudgetCategoryItem) => {
+			const objectKeys = Object.keys(element);
+			const found = objectKeys.findIndex(element => {
+				return element === field;
+			});
+			console.log('found', found);
+			if (found) {
+				totalSum += Number(Object.values(element)[found]);
+			}
+		});
+		setter(totalSum);
+	};
+
+	const sumDifference = () => {
+		let totalDifference = 0;
+		budgetData.forEach((element: BudgetCategoryItem) => {
+			totalDifference += element.planned - element.actual;
+		});
+		setTotalDiff(totalDifference);
+	};
+
+	const renderHeadColumns = () => {
+		return headColumns.map((headColumn: string, index) => {
+			return (
+				<th key={index} className="table-head-element">
+					<div>{handleColumnSwitch(headColumn)}</div>
+				</th>
+			);
+		});
+	};
+
+	const handleColumnSwitch = (columnName: string) => {
+		switch (columnName) {
+			case 'Planned':
+				return (
+					<>
+						<p>{columnName}</p>
+						<p>${totalPlanned}</p>
+					</>
+				);
+			case 'Actual':
+				return (
+					<>
+						<p>{columnName}</p>
+						<p>${totalActual}</p>
+					</>
+				);
+			case 'Difference':
+				return (
+					<>
+						<p>{columnName}</p>
+						<p className={`${totalDiff >= 0 ? 'green-text' : 'red-text'}`}>
+							${totalDiff}
+						</p>
+					</>
+				);
+			default:
+				return <p>{columnName}</p>;
+		}
+	};
+
+	const renderData = () => {
+		return budgetData.map((element: BudgetCategoryItem) => {
+			const diff = element.planned - element.actual;
+			return (
+				<tr key={element.id} className="table-row">
+					<td className="table-row-element">{element.title}</td>
+					<td className="table-row-element">${element.planned}</td>
+					<td className="table-row-element">${element.actual}</td>
+					<td className={`table-row-element ${diff >= 0 ? 'green-text' : 'red-text'}`}>
+						${element.planned - element.actual}
+					</td>
+				</tr>
+			);
+		});
+	};
+
+	return (
+		<>
+			<div className="table-title-container">
+				<h2>{title}</h2>
+				<button className="edit-button" onClick={() => onEdit()}>
+					<img className="edit-icon" src={editLogo} />
+				</button>
+			</div>
+			<div className="table-parent">
+				<table className="table-container">
+					<thead className="table-head">{renderHeadColumns()}</thead>
+					<tbody className="table-body">{renderData()}</tbody>
+				</table>
+			</div>
+		</>
+	);
+};
+export default BudgetCategoryTable;
--- src/components/views/Budgeting.tsx
+++ src/components/views/Budgeting.tsx
@@ -2,8 +2,29 @@ import {useEffect, useState} from 'react';
 import '@/styles/budgeting.css';
 import BudgetDetails from '@/components/organisms/budgeting/BudgetDetails';
 import SelectBudget from '@/components/organisms/budgeting/SelectBudget';
+import BudgetCategoryTable from '../organisms/budgeting/BudgetCategoryTable';
+import BudgetCategoryItem from '@/types/BudgetCategoryItem';
+
+const monthList = [
+	'Jan',
+	'Feb',
+	'Mar',
+	'Apr',
+	'May',
+	'Jun',
+	'Jul',
+	'Aug',
+	'Sep',
+	'Oct',
+	'Nov',
+	'Dec',
+];
 
 const Budgeting = () => {
+	const [budgetExists, setBudgetExists] = useState<boolean>(false);
+	const [selectedYear, setSelectedYear] = useState<number>(new Date().getFullYear());
+	const [selectedMonth, setSelectedMonth] = useState<number>(new Date().getMonth());
+
 	// useEffect(() => {
 	// 	const monthNumber = getMonthNumber(selectedMonth);
 	// 	fetch(`http://127.0.0.1:3000/budgeting/create/${selectedYear}/${monthNumber}`, {
@@ -35,49 +56,98 @@ const Budgeting = () => {
 	// 		});
 	// };
 
-	// const getMonthNumber = (inputMonth: string) => {
-	// 	const monthIndex = monthList.findIndex((month: string) => {
-	// 		return month === inputMonth;
-	// 	});
-	// 	return monthIndex + 1;
-	// };
+	const getMonthNumber = (inputMonth: string) => {
+		const monthIndex = monthList.findIndex((month: string) => {
+			return month === inputMonth;
+		});
+		return monthIndex + 1;
+	};
 
-	// const getMonthIndex = (inputMonth: string) => {
-	// 	return monthList.findIndex((month: string) => {
-	// 		return month === inputMonth;
-	// 	});
-	// };
+	const getMonthIndex = (inputMonth: string) => {
+		return monthList.findIndex((month: string) => {
+			return month === inputMonth;
+		});
+	};
+
+	const sampleData: BudgetCategoryItem[] = [
+		{id: 1, title: 'Food', planned: 100, actual: 250},
+		{id: 2, title: 'Groceries', planned: 250, actual: 100},
+		{id: 3, title: 'Other', planned: 50, actual: 100},
+		{id: 1, title: 'Food', planned: 100, actual: 250},
+		{id: 2, title: 'Groceries', planned: 250, actual: 100},
+		{id: 2, title: 'Groceries', planned: 250, actual: 100},
+		{id: 2, title: 'Groceries', planned: 250, actual: 100},
+		{id: 2, title: 'Groceries', planned: 250, actual: 100},
+		{id: 2, title: 'Groceries', planned: 250, actual: 100},
+		{id: 2, title: 'Groceries', planned: 250, actual: 100},
+		{id: 2, title: 'Groceries', planned: 250, actual: 100},
+		{id: 1, title: 'Food', planned: 100, actual: 250},
+		{id: 2, title: 'Groceries', planned: 250, actual: 100},
+		{id: 3, title: 'Other', planned: 50, actual: 100},
+		{id: 1, title: 'Food', planned: 100, actual: 250},
+		{id: 2, title: 'Groceries', planned: 250, actual: 100},
+		{id: 2, title: 'Groceries', planned: 250, actual: 100},
+		{id: 2, title: 'Groceries', planned: 250, actual: 100},
+		{id: 2, title: 'Groceries', planned: 250, actual: 100},
+		{id: 2, title: 'Groceries', planned: 250, actual: 100},
+		{id: 2, title: 'Groceries', planned: 250, actual: 100},
+		{id: 2, title: 'Groceries', planned: 250, actual: 100},
+		{id: 1, title: 'Food', planned: 100, actual: 250},
+		{id: 2, title: 'Groceries', planned: 250, actual: 100},
+		{id: 3, title: 'Other', planned: 50, actual: 100},
+		{id: 1, title: 'Food', planned: 100, actual: 250},
+		{id: 2, title: 'Groceries', planned: 250, actual: 100},
+		{id: 2, title: 'Groceries', planned: 250, actual: 100},
+		{id: 2, title: 'Groceries', planned: 250, actual: 100},
+		{id: 2, title: 'Groceries', planned: 250, actual: 100},
+		{id: 2, title: 'Groceries', planned: 250, actual: 100},
+		{id: 2, title: 'Groceries', planned: 250, actual: 100},
+		{id: 2, title: 'Groceries', planned: 250, actual: 100},
+	];
+
+	const triggerEdit = () => {};
 
 	return (
 		<div className="parent">
 			<div className="header">
 				<h1>Budgeting</h1>
+				{budgetExists ? <h2>{`${monthList[selectedMonth]} ${selectedYear}`}</h2> : <></>}
 			</div>
 			<div className="container">
-				{/* <div className="content">
-					
-					<div></div>
-				</div>*/}
-
-				<SelectBudget />
+				{!budgetExists ? (
+					<SelectBudget
+						onSubmit={() => {
+							setBudgetExists(true);
+						}}
+						passSelectedYear={(passedYear: number) => setSelectedYear(passedYear)}
+						passSelectedMonth={(passedMonth: number) => setSelectedMonth(passedMonth)}
+					/>
+				) : (
+					<>
+						<div className="category-section">
+							<BudgetCategoryTable
+								title={'Expenses'}
+								budgetData={sampleData}
+								onEdit={() => triggerEdit()}
+							/>
+						</div>
+						<div className="category-section">
+							<BudgetCategoryTable
+								title={'Monthly Payments'}
+								budgetData={sampleData}
+								onEdit={() => triggerEdit()}
+							/>
+						</div>
+						<div className="category-section">
+							<BudgetCategoryTable
+								title={'Income'}
+								budgetData={sampleData}
+								onEdit={() => triggerEdit()}
+							/>
+						</div>
+					</>
+				)}
 			</div>
-			{/*
-			{displayBudget ? (
-				<BudgetDetails
-					propsSelectedYear={selectedYear}
-					propsSelectedMonth={getMonthIndex(selectedMonth)}
-				/>
-			) : (
-				<div>
-					<h1>No budget for this month</h1>
-					<button
-						onClick={() => {
-							handleCreateBudget();
-						}}>
-						Create budget for this month
-					</button>
-				</div>
-			)} */}
 		</div>
 	);
 };
